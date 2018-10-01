7adbf0b576facdcedaf94cc974814d5a14dc21a1
Chris Powell <chris.ian.powell@gmail.com>
2018-10-01 13:00:02 -0600

    adds complete refactor

--- script.js
+++ script.js
@@ -2,13 +2,20 @@ var number = 0
 var counter = 0;
 var min_numb = 0;
 var max_numb = 100;
+var clear_button = document.getElementById('clear_button');
+var reset_button = document.getElementById('reset_button');
+var range_button = document.getElementById('range_button');
+var minMax = document.getElementById('min_max');
+var numberField = document.getElementById('number_field');
+var guessNumber = document.getElementById('guess_number');
 
-document.getElementById('clear_button').disabled = true;
-document.getElementById('clear_button').style.backgroundColor = '#D0D2D3'
-document.getElementById('reset_button').disabled = true;
-document.getElementById('reset_button').style.backgroundColor = '#D0D2D3'
+var answerLineOne = document.getElementById('answer-1');
+var answerLineTwo = document.getElementById('answer-2');
+var answerLineNum = document.getElementById('answer-num');
+var answerLineThree = document.getElementById('answer-3');
+var answerLineFour = document.getElementById('answer-4');
 
-document.getElementById('range_button').onclick = function(){
+function getUserRange() {
   min_numb = document.getElementById('min_number').value;
   max_numb = document.getElementById('max_number').value;
 
@@ -18,108 +25,124 @@ document.getElementById('range_button').onclick = function(){
   number = Math.floor(Math.random() * (max_numb - min_numb) + min_numb);
 
   if (min_numb > max_numb) {
-    document.getElementById('answer-1').innerHTML = 'The Minimum Value Is';
-    document.getElementById('answer-2').innerHTML = 'Higher Than The Maximum';
-    document.getElementById('answer-3').innerHTML = 'Please Input New Values';
-    document.getElementById('answer-4').innerHTML = 'For Minimum and Maximum';
+    answerLineOne.innerHTML = 'The Minimum Value Is';
+    answerLineTwo.innerHTML = 'Higher Than The Maximum';
+    answerLineThree.innerHTML = 'Please Input New Values';
+    answerLineFour.innerHTML = 'For Minimum and Maximum';
   } else if (isNaN(min_numb) || isNaN(max_numb)){
-    document.getElementById('answer-1').innerHTML = 'One Of The Values You Have';
-    document.getElementById('answer-2').innerHTML = 'Entered Was Not A Number.';
-    document.getElementById('answer-3').innerHTML = 'Please Enter New Minimum';
-    document.getElementById('answer-4').innerHTML = 'And Maximum Values';
+    answerLineOne.innerHTML = 'One Of The Values You Have';
+    answerLineTwo.innerHTML = 'Entered Was Not A Number.';
+    answerLineThree.innerHTML = 'Please Enter New Minimum';
+    answerLineFour.innerHTML = 'And Maximum Values';
   } else {
-    document.getElementById('answer-1').innerHTML = 'Try To Guess The Number';
-    document.getElementById('answer-2').innerHTML = '';
-    document.getElementById('answer-3').innerHTML = '';
-    document.getElementById('answer-4').innerHTML = '';
-    document.getElementById('min_max').style.display = "none"
-    document.getElementById('reset_button').disabled = false;
-    document.getElementById('reset_button').style.backgroundColor = '#929497'
+    answerLineOne.innerHTML = 'Try To Guess The Number';
+    answerLineTwo.innerHTML = '';
+    answerLineThree.innerHTML = '';
+    answerLineFour.innerHTML = '';
+    minMax.style.display = "none"
+    reset_button.disabled = false;
+    reset_button.style.backgroundColor = '#929497'
   }
 };
 
-document.getElementById('number_field').onkeyup = function(){
-  if (document.getElementById('number_field').value.length == 0) {
-    document.getElementById('clear_button').disabled = true;
-    document.getElementById('clear_button').style.backgroundColor = '#D0D2D3'
+function enableClearButton() {
+  if (numberField.value.length == 0) {
+    clear_button.disabled = true;
+    clear_button.style.backgroundColor = '#D0D2D3'
   } else {
-    document.getElementById('clear_button').disabled = false;
-    document.getElementById('clear_button').style.backgroundColor = '#929497';
+    clear_button.disabled = false;
+    clear_button.style.backgroundColor = '#929497';
   }
 };
 
-document.getElementById('clear_number').onclick = function(){
-  document.getElementById('number_field').value = ''
-  document.getElementById('clear_button').disabled = true;
-  document.getElementById('clear_button').style.backgroundColor = '#D0D2D3'
+function clearNumber() {
+  numberField.value = ''
+  clear_button.disabled = true;
+  clear_button.style.backgroundColor = '#D0D2D3'
 };
 
-document.getElementById('guess_number').onclick = function(){
-  document.getElementById('clear_button').disabled = true;
-  document.getElementById('clear_button').style.backgroundColor = '#D0D2D3';
+function guessGamePlay() {
+  clear_button.disabled = true;
+  clear_button.style.backgroundColor = '#D0D2D3';
   counter++;
 
-  var guess = document.getElementById('number_field').value;
+  var guess = numberField.value;
 
   var guessedNumber = parseInt(guess);
 
   if (isNaN(guessedNumber)) {
-    document.getElementById('answer-1').innerHTML = 'That Is Not A Number'
-    document.getElementById('answer-num').innerHTML = ''
-    document.getElementById('answer-2').innerHTML = ''
-    document.getElementById('answer-3').innerHTML = ''
-    document.getElementById('answer-4').innerHTML = ''
+    answerLineOne.innerHTML = 'That Is Not A Number'
+    answerLineNum.innerHTML = ''
+    answerLineTwo.innerHTML = ''
+    answerLineThree.innerHTML = ''
+    answerLineFour.innerHTML = ''
   }
   else if (guessedNumber > max_numb) {
-    document.getElementById('answer-1').innerHTML = 'You Last Guess Was'
-    document.getElementById('answer-2').innerHTML = ''
-    document.getElementById('answer-num').innerHTML = `${guessedNumber}`
-    document.getElementById('answer-3').innerHTML = 'Which Is Higher than the Possible Maximum Value'
-    document.getElementById('answer-4').innerHTML = ''
+    answerLineOne.innerHTML = 'You Last Guess Was'
+    answerLineTwo.innerHTML = ''
+    answerLineNum.innerHTML = `${guessedNumber}`
+    answerLineThree.innerHTML = 'Which Is Higher than the Possible Maximum Value'
+    answerLineFour.innerHTML = ''
   }
   else if (guessedNumber < min_numb) {
-    document.getElementById('answer-1').innerHTML = 'You Last Guess Was'
-    document.getElementById('answer-2').innerHTML = ''
-    document.getElementById('answer-num').innerHTML = `${guessedNumber}`
-    document.getElementById('answer-3').innerHTML = 'Which Is Lower than the Possible Maximum Value'
-    document.getElementById('answer-4').innerHTML = ''
+    answerLineOne.innerHTML = 'You Last Guess Was'
+    answerLineTwo.innerHTML = ''
+    answerLineNum.innerHTML = `${guessedNumber}`
+    answerLineThree.innerHTML = 'Which Is Lower than the Possible Maximum Value'
+    answerLineFour.innerHTML = ''
   }
   else if (guessedNumber > number) {
-    document.getElementById('answer-1').innerHTML = 'You Last Guess Was'
-    document.getElementById('answer-2').innerHTML = ''
-    document.getElementById('answer-num').innerHTML = `${guessedNumber}`
-    document.getElementById('answer-3').innerHTML = 'Which is too High'
-    document.getElementById('answer-4').innerHTML = ''
+    answerLineOne.innerHTML = 'You Last Guess Was'
+    answerLineTwo.innerHTML = ''
+    answerLineNum.innerHTML = `${guessedNumber}`
+    answerLineThree.innerHTML = 'Which is too High'
+    answerLineFour.innerHTML = ''
   }
   else if (guessedNumber < number) {
-    document.getElementById('answer-1').innerHTML = 'You Last Guess Was'
-    document.getElementById('answer-2').innerHTML = ''
-    document.getElementById('answer-num').innerHTML = `${guessedNumber}`
-    document.getElementById('answer-3').innerHTML = 'Which is too low'
-    document.getElementById('answer-4').innerHTML = ''
+    answerLineOne.innerHTML = 'You Last Guess Was'
+    answerLineTwo.innerHTML = ''
+    answerLineNum.innerHTML = `${guessedNumber}`
+    answerLineThree.innerHTML = 'Which is too low'
+    answerLineFour.innerHTML = ''
   }
   else {
-    document.getElementById('answer-1').innerHTML = 'BOOM!'
-    document.getElementById('answer-num').innerHTML = `${guessedNumber}`
-    document.getElementById('answer-2').innerHTML = `You Got The Answer in ${counter} Guesses`
-    document.getElementById('answer-3').innerHTML = 'A New Possible Answer is Waiting to be Discovered'
-    document.getElementById('answer-4').innerHTML = 'The Max Has Been Increased By Ten And The Min has been decreased by Ten'
+    answerLineOne.innerHTML = 'BOOM!'
+    answerLineNum.innerHTML = `${guessedNumber}`
+    answerLineTwo.innerHTML = `You Got The Answer in ${counter} Guesses`
+    answerLineThree.innerHTML = 'A New Possible Answer is Waiting to be Discovered'
+    answerLineFour.innerHTML = 'The Max Has Been Increased By Ten And The Min has been decreased by Ten'
     max_numb = max_numb + 10
     min_numb = min_numb - 10
     number = Math.floor(Math.random() * (max_numb - min_numb) + min_numb);
     counter = 0
   }
-  document.getElementById('number_field').value = ''
+  numberField.value = ''
 };
 
-document.getElementById('reset_button').onclick = function(){
+function resetGame() {
   counter = 0;
-  document.getElementById('reset_button').disabled = true;
-  document.getElementById('reset_button').style.backgroundColor = '#D0D2D3'
-  document.getElementById('answer-1').innerHTML = 'The Game Has Been Reset';
-  document.getElementById('answer-num').innerHTML = '';
-  document.getElementById('answer-2').innerHTML = 'The Maximum Possible Answer';
-  document.getElementById('answer-3').innerHTML = 'Has Been Reset To Ten';
-  document.getElementById('answer-4').innerHTML = ''
-  document.getElementById('min_max').style.display = "block";
+  reset_button.disabled = true;
+  reset_button.style.backgroundColor = '#D0D2D3'
+  answerLineOne.innerHTML = 'The Game Has Been Reset';
+  answerLineNum.innerHTML = '';
+  answerLineTwo.innerHTML = 'The Maximum Possible Answer';
+  answerLineThree.innerHTML = 'Has Been Reset To Ten';
+  answerLineFour.innerHTML = ''
+  minMax.style.display = "block";
 };
+
+clear_button.disabled = true;
+clear_button.style.backgroundColor = '#D0D2D3'
+
+reset_button.disabled = true;
+reset_button.style.backgroundColor = '#D0D2D3'
+
+range_button.onclick = function(){getUserRange()}
+
+numberField.onkeyup = function(){enableClearButton()};
+
+clear_button.onclick = function(){clearNumber()};
+
+guessNumber.onclick = function(){guessGamePlay()};
+
+reset_button.onclick = function(){resetGame()};
